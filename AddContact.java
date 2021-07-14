16892afb35f7ef1d22c5779bc14236d8ad1e96b8
abhi111676 <abhi111676@gmail.com>
2021-07-14 15:45:07 +0530

    Added a code that <Adds New Contact>

---
+++ AddContact.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class AddContact {
+
+	public static void main(String[] args) throws IOException {
+		System.out.println("To Add A New Contact To Address Book ");
+		InputStreamReader read = new InputStreamReader(System.in);
+		BufferedReader in = new BufferedReader(read);
+
+		System.out.println("Enter first name:- ");
+		String fname = in.readLine();
+
+		System.out.println("Enter last name:- ");
+		String lname = in.readLine();
+
+		System.out.println("Address :-");
+
+		String address = in.readLine();
+
+		System.out.println("State :-");
+		String state = in.readLine();
+
+		System.out.println("Zip code:- ");
+		String zip = in.readLine();
+
+		System.out.println("Phone number:- ");
+		String phone = in.readLine();
+
+		System.out.println("Email Id:- ");
+		String emailId = in.readLine();
+
+		System.out.println("Name: " + fname + " " + lname);
+
+		System.out.println("Address : " + address);
+		System.out.println("State : " + state);
+		System.out.println("Zip code : " + zip);
+		System.out.println("Phone number : " + phone);
+		System.out.println("Email Id :" + emailId);
+
+		System.out.println("Contact Added Successfully");
+	}
+
+}
